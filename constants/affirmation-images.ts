9a858f1f829c0263d8cd080487ed8845049c3b76
tct123 <42028373+tct123@users.noreply.github.com>
2025-09-16 01:29:09 +0200

    Update affirmation-images.ts

--- constants/affirmation-images.ts
+++ constants/affirmation-images.ts
@@ -1,30 +1,60 @@
+const californiaBackyardOne = require("@/assets/affirmation-images/California-backyard-1.webp")
+
+const californiaBackyardTwo = require("@/assets/affirmation-images/California-backyard-2.webp")
+const californiaBackyardThree = require("@/assets/affirmation-images/California-backyard-3.webp")
+const californiaBackyardFour = require("@/assets/affirmation-images/California-backyard-4.webp")
+const englishCountrysideOne = require("@/assets/affirmation-images/english-countryside-1.webp")
+const englishCountrysideTwo = require("@/assets/affirmation-images/english-countryside-2.webp")
+const englishCountrysideThree = require("@/assets/affirmation-images/english-countryside-3.webp")
+const englishCountrysideFour = require("@/assets/affirmation-images/english-countryside-4.webp")
+const mountainMeditateOne = require("@/assets/affirmation-images/mountain-meditate-1.webp")
+const mountainMeditateTwo = require("@/assets/affirmation-images/mountain-meditate-2.webp")
+const mountainMeditateThree = require("@/assets/affirmation-images/mountain-meditate-3.webp")
+const mountainMeditateFour = require("@/assets/affirmation-images/mountain-meditate-4.webp")
+const nightSkyOne = require("@/assets/affirmation-images/night-sky-1.webp")
+const nightSkyTwo = require("@/assets/affirmation-images/night-sky-2.webp")
+const nightSkyThree = require("@/assets/affirmation-images/night-sky-3.webp")
+const nightSkyFour = require("@/assets/affirmation-images/night-sky-4.webp")
+const oregonOne = require("@/assets/affirmation-images/oregon-1.webp")
+const oregonTwo = require("@/assets/affirmation-images/oregon-2.webp")
+const oregonThree = require("@/assets/affirmation-images/oregon-3.webp")
+const oregonFour = require("@/assets/affirmation-images/oregon-4.webp")
+const relaxingRiverOne = require("@/assets/affirmation-images/relaxing-river-1.webp")
+const relaxingRiverTwo = require("@/assets/affirmation-images/relaxing-river-2.webp")
+const relaxingRiverThree = require("@/assets/affirmation-images/relaxing-river-3.webp")
+const relaxingRiverFour = require("@/assets/affirmation-images/relaxing-river-4.webp")
+const tuscannyOne = require("@/assets/affirmation-images/Tuscanny-1.webp")
+const tuscannyTwo = require("@/assets/affirmation-images/Tuscanny-2.webp")
+const tuscannyThree = require("@/assets/affirmation-images/Tuscanny-3.webp")
+const tuscannyFour = require("@/assets/affirmation-images/Tuscanny-4.webp")
+
 export default {
-    let californiaBackyardOne = require("@/assets/affirmation-images/California-backyard-1.webp"),
-    let californiaBackyardTwo = require("@/assets/affirmation-images/California-backyard-2.webp"),
-    let californiaBackyardThree= require("@/assets/affirmation-images/California-backyard-3.webp"),
-    let californiaBackyardFour= require("@/assets/affirmation-images/California-backyard-4.webp"),
-    let englishCountrysideOne= require("@/assets/affirmation-images/english-countryside-1.webp"),
-    let englishCountrysideTwo= require("@/assets/affirmation-images/english-countryside-2.webp"),
-    let englishCountrysideThree= require("@/assets/affirmation-images/english-countryside-3.webp"),
-    let englishCountrysideFour= require("@/assets/affirmation-images/english-countryside-4.webp"),
-    let mountainMeditateOne= require("@/assets/affirmation-images/mountain-meditate-1.webp"),
-    let mountainMeditateTwo= require("@/assets/affirmation-images/mountain-meditate-2.webp"),
-    let mountainMeditateThree= require("@/assets/affirmation-images/mountain-meditate-3.webp"),
-    let mountainMeditateFour= require("@/assets/affirmation-images/mountain-meditate-4.webp"),
-    let nightSkyOne= require("@/assets/affirmation-images/night-sky-1.webp"),
-    let nightSkyTwo= require("@/assets/affirmation-images/night-sky-2.webp"),
-    let nightSkyThree= require("@/assets/affirmation-images/night-sky-3.webp"),
-    let nightSkyFour= require("@/assets/affirmation-images/night-sky-4.webp"),
-    let oregonOne= require("@/assets/affirmation-images/oregon-1.webp"),
-    let oregonTwo= require("@/assets/affirmation-images/oregon-2.webp"),
-    let oregonThree= require("@/assets/affirmation-images/oregon-3.webp"),
-    let oregonFour= require("@/assets/affirmation-images/oregon-4.webp"),
-    let relaxingRiverOne= require("@/assets/affirmation-images/relaxing-river-1.webp"),
-    let relaxingRiverTwo= require("@/assets/affirmation-images/relaxing-river-2.webp"),
-    let relaxingRiverThree= require("@/assets/affirmation-images/relaxing-river-3.webp"),
-    let relaxingRiverFour= require("@/assets/affirmation-images/relaxing-river-4.webp"),
-    let tuscannyOne= require("@/assets/affirmation-images/Tuscanny-1.webp"),
-    let tuscannyTwo= require("@/assets/affirmation-images/Tuscanny-2.webp"),
-    let tuscannyThree= require("@/assets/affirmation-images/Tuscanny-3.webp"),
-    let tuscannyFour= require("@/assets/affirmation-images/Tuscanny-4.webp"),
+    californiaBackyardOne,
+    californiaBackyardTwo,
+    californiaBackyardThree,
+    californiaBackyardFour,
+    englishCountrysideOne,
+    englishCountrysideTwo,
+    englishCountrysideThree,
+    englishCountrysideFour,
+    mountainMeditateOne,
+    mountainMeditateTwo,
+    mountainMeditateThree,
+    mountainMeditateFour,
+    nightSkyOne,
+    nightSkyTwo,
+    nightSkyThree,
+    nightSkyFour,
+    oregonOne,
+    oregonTwo,
+    oregonThree,
+    oregonFour,
+    relaxingRiverOne,
+    relaxingRiverTwo,
+    relaxingRiverThree,
+    relaxingRiverFour,
+    tuscannyOne,
+    tuscannyTwo,
+    tuscannyThree,
+    tuscannyFour
 };
